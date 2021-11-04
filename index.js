b2932ec9825d9cbe73920f2f7dfabd71d5766431
强丹蕾 <qiangdanlei@qiangdaleideMBP.hitronhub.home>
2021-11-04 02:36:47 -0700

    add function

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
-/* GET home page. */
-app.get('/', (req, res) =>{
-    res.send({hi : 'there'});
-});
+require('./models/User');
+require('./services/passport');
 
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
